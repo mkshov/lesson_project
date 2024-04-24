ff040717ff08a19f6f707fb789a8b11a048f3643
aselya020407@gmail.com <aselya020407@gmail.com>
2024-04-24 21:38:49 +0600

    added edit & details functions

--- main.js
+++ main.js
@@ -20,15 +20,41 @@ let inpFirstPrice = document.getElementById("add-price-1");
 let inpSecondPrice = document.getElementById("add-price-2");
 let addProductBtn = document.getElementById("add-btn");
 
+// ? На кнопку навесили события чтоб открыть модалку
 openAddFormBtn.addEventListener("click", () => {
   addModal.style.display = "flex";
 });
 
+// ? Закрытие модалки при клике на крестик внутри нее
 closeAddForm.addEventListener("click", () => {
   addModal.style.display = "none";
 });
 
+// ? начало details
+let closeDetailsBtn = document.getElementById("close-details");
+let detailsParent = document.getElementById("details");
+let detailsInner = document.getElementById("details-inner");
+closeDetailsBtn.addEventListener("click", () => {
+  detailsParent.style.display = "none";
+});
+
+// ! for edit
+let editModal = document.getElementById("edit-modal");
+let editModalInner = document.getElementById("edit-modal-inner");
+let closeEditModal = document.getElementById("close-edit-modal");
+let inpImageEdit = document.getElementById("edit-img");
+let inpDescriptionEdit = document.getElementById("edit-description");
+let inpFirstPriceEdit = document.getElementById("edit-price-1");
+let inpSecondPriceEdit = document.getElementById("edit-price-2");
+let editProductBtn = document.getElementById("edit-btn");
+
+closeEditModal.addEventListener("click", () => {
+  editModal.style.display = "none";
+});
+
+// ? При клике на кнопку внутри модалки, собираем данные с инпута в объект и делаем запрос на добавление продукта, после скрываем модалку
 addProductBtn.addEventListener("click", async () => {
+  // ? собираем данные
   let product = {
     image: inpImage.value,
     description: inpDescription.value,
@@ -36,21 +62,26 @@ addProductBtn.addEventListener("click", async () => {
     second_price: inpSecondPrice.value,
   };
 
-  await fetch(API, {
-    method: "POST",
-    body: JSON.stringify(product),
-    headers: {
-      "Content-type": "application/json; charset=utf-8",
-    },
-  });
+  // ? отправляем данные
+  if (inpImage.value.trim() && inpDescription.value.trim() && inpFirstPrice.value.trim() && inpSecondPrice.value.trim()) {
+    await fetch(API, {
+      method: "POST",
+      body: JSON.stringify(product),
+      headers: {
+        "Content-type": "application/json; charset=utf-8",
+      },
+    });
+  } else {
+    alert("Заполните поля!");
+  }
+
+  // ? закрываем модалку
   addModal.style.display = "none";
 });
 
 async function showProducts() {
   let products = await fetch(API).then((res) => res.json());
   console.log("products: ", products);
-
-  productsList.innerHTML = "";
 
   products.forEach((product, id) => {
     let div = document.createElement("div");
@@ -65,8 +96,77 @@ async function showProducts() {
     </div>
     `;
 
+    // ! details
+    div.addEventListener("click", () => {
+      detailsInner.innerHTML = "";
+      detailsParent.style.display = "flex";
+
+      detailsInner.innerHTML = `
+      <img src="${product.image}"/>
+      <div class="details-text" id="details-text">
+        <p>${product.description}</p>
+        <div class="card-price">
+            <p>${product.first_price}сом</p>
+            <p>${product.second_price}сом</p>
+        </div>
+      </div>
+      `;
+      let detailsText = document.getElementById("details-text");
+      let btnDelete = document.createElement("button");
+      btnDelete.innerHTML = "Удалить продукт";
+
+      btnDelete.addEventListener("click", (e) => deleteProduct(product.id, e));
+
+      // ! создаем кнопку для редактирования внутри details
+      let editBtn = document.createElement("button");
+      editBtn.innerHTML = "Редактировать продукт";
+      detailsText.append(btnDelete, editBtn);
+
+      editBtn.addEventListener("click", (e) => {
+        editModal.style.display = "flex";
+        editModal.style.zIndex = "2";
+        editProduct(product);
+      });
+    });
+
+    // ! edit
+
     productsList.append(div);
   });
 }
 
 showProducts();
+
+async function deleteProduct(id, event) {
+  event.stopPropagation();
+  await fetch(`${API}/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json; charset=utf-8",
+    },
+  });
+  detailsParent.style.display = "none";
+}
+
+function editProduct(product) {
+  inpImageEdit.value = product.image;
+  inpDescriptionEdit.value = product.description;
+  inpFirstPriceEdit.value = product.first_price;
+  inpSecondPriceEdit.value = product.second_price;
+
+  editProductBtn.addEventListener("click", async () => {
+    let newProduct = {
+      image: inpImageEdit.value,
+      description: inpDescriptionEdit.value,
+      first_price: inpFirstPriceEdit.value,
+      second_price: inpSecondPriceEdit.value,
+    };
+    await fetch(`${API}/${product.id}`, {
+      method: "PATCH",
+      body: JSON.stringify(newProduct),
+      headers: {
+        "Content-type": "application/json; charset=utf-8",
+      },
+    });
+  });
+}
